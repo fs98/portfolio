16a2b50ea5a71314d23d81eca07d7b733e147146
f_sefer <sefer.fata@gmail.com>
2020-02-24 00:00:40 +0100

    no message

---
+++ index.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<!-- [Meta tags] -->
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="icon" type="image/ico" href="img/logo.png"/>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+	
+
+	<title>Hi, I'm Fata.</title>
+</head>
+<body>
+
+<!-- [Navbar] -->
+
+	<nav class="navbar navbar-expand-lg sticky-top bg-linear">
+		<div class="container px-0">
+			  <a class="navbar-brand" href="#">
+		    	<img src="img/logo.png" height="30" alt="" class="d-inline-block align-top"> <span class="font-weight-500 text-white">fs</span>
+		  	</a>
+			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			    <span class="navbar-toggler-icon d-flex justify-content-center align-items-center px-0 mx-0"><img src="img/icons/toggler.png"></span>
+			  </button>
+			  <div class="collapse navbar-collapse m-auto" id="navbarNav">
+			    <ul class="navbar-nav ml-auto">
+			      <li class="nav-item mr-xl-3 mr-lg-3 mt-lg-0 mt-xl-0 mt-md-0 mt-md-2 mt-sm-2 mt-2 active px-1">
+			        <a class="nav-link text-uppercase font-weight-600 font-size-14 text-xl-left text-lg-left text-md-center text-sm-center text-center" href="#">Home</a>
+			      </li>
+			      <li class="nav-item mr-xl-3 mr-lg-3 mt-lg-0 mt-xl-0 mt-md-0 mt-md-2 mt-sm-2 mt-2 px-1">
+			        <a class="nav-link text-uppercase font-weight-600 font-size-14 text-xl-left text-lg-left text-md-center text-sm-center text-center" href="#">Service</a>
+			      </li>
+			      <li class="nav-item mr-xl-3 mr-lg-3 mt-lg-0 mt-xl-0 mt-md-0 mt-md-2 mt-sm-2 mt-2 px-1">
+			        <a class="nav-link text-uppercase font-weight-600 font-size-14 text-xl-left text-lg-left text-md-center text-sm-center text-center" href="#">About</a>
+			      </li>
+			      <li class="nav-item mr-xl-3 mr-lg-3 mt-lg-0 mt-xl-0 mt-md-0 mt-md-2 mt-sm-2 mt-2 px-1">
+			        <a class="nav-link text-uppercase font-weight-600 font-size-14 text-xl-left text-lg-left text-md-center text-sm-center text-center" href="#">Projects</a>
+			      </li>
+			      <li class="nav-item my-lg-0 my-xl-0 my-md-0 my-md-2 my-sm-2 my-2 px-1">
+			        <a class="nav-link text-uppercase font-weight-600 font-size-14 text-xl-left text-lg-left text-md-center text-sm-center text-center pr-0" href="#">Contact</a>
+			      </li>
+			    </ul>
+			  </div>
+		</div>
+	</nav>
+
+<!-- Hi Container -->
+
+<section class="bg-linear pt-xl-5 pb-xl-0 pt-lg-5 pb-lg-0 pt-md-5 pb-md-0 py-sm-5 py-4 px-0" id="hi-container">
+	<div class="container pt-xl-5 pb-xl-0 pt-lg-5 pb-lg-0 pt-md-5 pb-md-0 py-sm-4 py-3">
+		<div class="row py-xl-0 py-lg-0 py-md-0 py-sm-3 py-3">
+			<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 d-flex align-items-xl-end align-items-lg-end align-items-md-center justify-content-md-center justify-content-sm-center justify-content-center mb-0">
+					<img src="img/fata.png" class="img-fluid mb-xl-0 mb-lg-0 mb-md-0 mb-sm-5 mb-5">
+			</div>
+			<div class="col-xl-6 mx-0 col-lg-6 col-md-6 col-sm-12 col-12 d-flex align-items-center justify-content-center">
+				<div class="text-xl-left text-lg-left text-md-left text-sm-center text-center">
+					<h1 class="font-weight-100 text-white">Hi, I'm <br class="d-xl-blocl d-lg-block d-md-block d-sm-none d-block"><span class="text-uppercase font-weight-bold">Fata Sefer</span><br>Web Developer</h1>
+					<a href="#" class="btn text-ebony-clay font-size-14 text-uppercase font-weight-600 px-5 py-2">Talk to me</a>
+				</div>
+			</div>
+		<div>
+	</div>
+</section>
+
+<!-- [End of Hi Container] -->
+
+<!-- [Services Container] -->
+
+<section id="services-container">
+	<div class="container py-5 px-xl-0 px-lg-0 px-md-0 px-sm-0">
+		<div class="row py-5">
+			<h1 class="text-center w-100 font-weight-600">Take a Look at my Services</h1>
+			<h6 class="text-center w-100 font-weight-300 text-grey mt-1">I can use code to build whatever I want!</h6>
+		</div>
+		<div class="row">
+			<div class="col-xl-6 col-lg-6 col-md-6">
+				<img src="img/icons/monitor.png" class="img-fluid icon">
+				<h5 class="mt-4 service-title font-weight-600">Web Design</h5>
+				<p class="mt-4 font-weight-300 text-grey">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat.</p>
+			</div>
+			<div class="col-xl-6 col-lg-6 col-md-6">
+				<img src="img/icons/programming.png" class="img-fluid icon">
+				<h5 class="mt-4 service-title font-weight-600">Front-end development</h5>
+				<p class="mt-4 font-weight-300 text-grey">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat.</p>
+			</div>
+		</div>
+		<div class="row mt-4">
+			<div class="col-xl-6 col-lg-6 col-md-6">
+				<img src="img/icons/backend.png" class="img-fluid icon">
+				<h5 class="mt-4 service-title font-weight-600">Back-end development</h5>
+				<p class="mt-4 font-weight-300 text-grey">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat.</p>
+			</div>
+			<div class="col-xl-6 col-lg-6 col-md-6">
+				<img src="img/icons/computer.png" class="img-fluid icon">
+				<h5 class="mt-4 service-title font-weight-600">Database Management</h5>
+				<p class="mt-4 font-weight-300 text-grey">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat.</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- [End of Services Container] -->
+
+<!-- [About Me Container] -->
+
+<section class="bg-grey" id="about-me-container">
+	<div class="container py-5 px-xl-0 px-lg-0 px-md-0 px-sm-0">
+		<div class="row py-5">
+			<h1 class="text-center w-100 font-weight-600">Here's a little something about me</h1>
+			<h6 class="text-center w-100 font-weight-300 text-grey mt-1">Doing what you love is where happiness lives!</h6>
+		</div>
+		<div class="row mb-5">
+			<div class="col-xl-6 col-lg-6 col-md-12">
+				<p class="font-weight-300 text-grey">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat.</p>
+				<h5 class="font-weight-600 skills-title mt-4 mb-5">My Skills</h5>
+				<div class="progress-div">
+					<p class="mb-2 text-right text-grey font-weight-400">HTML 80%</p>
+					<div class="progress bg-white rounded-pill">
+					  <div class="progress-bar bg-linear rounded-pill" style="width: 80%;" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+					</div>
+				</div>
+				<div class="progress-div">
+					<p class="mb-2 text-right text-grey font-weight-400">CSS 75%</p>
+					<div class="progress bg-white rounded-pill">
+					  <div class="progress-bar bg-linear rounded-pill" style="width: 75%;" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+					</div>
+				</div>
+				<div class="progress-div">
+					<p class="mb-2 text-right text-grey font-weight-400">Bootstrap 85%</p>
+					<div class="progress bg-white rounded-pill">
+					  <div class="progress-bar bg-linear rounded-pill" style="width: 85%;" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+					</div>
+				</div>
+				<div class="progress-div">
+					<p class="mb-2 text-right text-grey font-weight-400">PHP 65%</p>
+					<div class="progress bg-white rounded-pill">
+					  <div class="progress-bar bg-linear rounded-pill" style="width: 65%;" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+					</div>
+				</div>
+				<div class="progress-div">
+					<p class="mb-2 text-right text-grey font-weight-400">MySql 65%</p>
+					<div class="progress bg-white rounded-pill">
+					  <div class="progress-bar bg-linear rounded-pill" style="width: 65%;" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xl-6 col-lg-6 col-md-12 mt-xl-0 mt-lg-0 mt-md-5 mt-sm-5 mt-5 d-flex justify-content-xl-end justify-content-lg-end justify-content-md-center justify-content-sm-center justify-content-center align-items-center">
+				<div class="card mt-xl-0 mt-lg-0 mt-md-5 mt-sm-5 mt-5 border-0">
+				  <img src="img/card-img-1.jpeg" class="card-img-top rounded-0" alt="...">
+				  <div class="card-body border-0 rounded-0">
+				  	<h5 class="card-title font-weight-600 text-center my-0">Fata Sefer</h5>
+				    <p class="card-text text-center text-grey font-weight-300">Web Developer</p>
+				    <div class="text-center">
+				    	<img src="img/icons/telephone.png" class="card-icon d-inline">&nbsp;<p class="d-inline text-grey font-weight-300">(+387) 62 808 723</p>
+				    </div>
+				    <div class="text-center mt-3">
+				    	<img src="img/icons/mail.png" class="card-icon d-inline">&nbsp;<p class="d-inline text-grey font-weight-300">sefer.fata@gmail.com</p>
+				    </div>
+				    <div class="text-center">
+				    	<a href="#" class="mt-4 mb-2 btn font-size-14 text-uppercase font-weight-400 px-5 py-2 bg-linear rounded-pill hire-me-btn">Hire me</a>
+				    </div>
+				  </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- [End of About Me Container] -->
+
+<!-- [Projects Container] -->
+
+<section id="projects-container">
+	<div class="container py-5 px-xl-0 px-lg-0 px-md-0 px-sm-0">
+		<div class="row py-5">
+			<h1 class="text-center w-100 font-weight-600">My Latest Projects</h1>
+			<h6 class="text-center w-100 font-weight-300 text-grey mt-1">Practice makes perfect!</h6>
+		</div>
+		<div class="row mb-5 pt-5">
+			<div class="col-xl-6 col-lg-6">
+				<img src="img/projects/hotel.png" class="img-fluid">
+			</div>
+			<div class="col-xl-6 col-lg-6 mt-5">
+				<h5 class="project-title font-weight-600">Hotel Website</h5>
+				<p class="text-grey font-weight-300 mt-3">One of the most important things for any business that is still in its infancy and looking for greener pastures is good web design. Web design can be that one thing that pushes you above or pulls you below the competition.</p>
+			</div>
+		</div>
+		<div class="row mb-5  pt-5">
+			<div class="col-xl-6 col-lg-6">
+				<img src="img/projects/foodblog.png" class="img-fluid">
+			</div>
+			<div class="col-xl-6 col-lg-6 mt-5">
+				<h5 class="project-title font-weight-600">Foodblog</h5>
+				<p class="text-grey font-weight-300 mt-3">Good food equals good mood. And now you can share with anyone you want - because it all starts with the design and presentation of text and visual content</p>
+			</div>
+		</div>
+		<div class="row my-5 pt-5">
+			<div class="col-xl-6 col-lg-6">
+				<img src="img/projects/online gradebook.png" class="img-fluid">
+			</div>
+			<div class="col-xl-6 col-lg-6 mt-5">
+				<h5 class="project-title font-weight-600">Online gradebook</h5>
+				<p class="text-grey font-weight-300 mt-3">This is an information system that provides records of students, their progress and grades, as well as absenteeism documentation and other data. Such an information system makes essential information easily accessible to parents and students, as well as teachers and others who need it. This project is still in making progress...</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- [End of Projects Container] -->
+
+<!-- [Contact Info Container] -->
+
+<section class="bg-grey pb-5" id="contact-container">
+	<div class="container py-5 px-xl-0 px-lg-0 px-md-0 px-sm-0">
+		<div class="row py-5">
+			<h1 class="text-center w-100 font-weight-600">If you have any question, fill out the form below.</h1>
+			<h6 class="text-center w-100 font-weight-300 text-grey mt-1">I'll be glad to help.</h6>
+		</div>
+		<form>
+			<div class="row">
+				<div class="col-xl-6 col-lg-6 col-md-12 form-group d-flex flex-column justify-content-between">
+				  <input type="text" class="form-control rounded-0 pl-3 mt-xl-0 mt-lg-0 mt-md-3 mt-sm-3 mt-3" placeholder="Name">
+				  <input type="text" class="form-control rounded-0 pl-3 mt-xl-0 mt-lg-0 mt-md-3 mt-sm-3 mt-3" id="inputEmail4" placeholder="Email address">
+				  <input type="text" class="form-control rounded-0 pl-3 mt-xl-0 mt-lg-0 mt-md-3 mt-sm-3 mt-3" id="inputAddress" placeholder="Subject*">
+				</div>
+				<div class="col-xl-6 col-lg-6 col-md-12 form-group d-flex d-md-block mt-xl-0 mt-lg-0 mt-md-3 mt-sm-4 mt-4">
+					<textarea class="form-control rounded-0" style="resize: none;" rows="9" id="exampleFormControlTextarea1" placeholder="Message"></textarea>
+				</div>
+				<div class="col-xl-6 offset-xl-6 col-lg-6 offset-lg-6">
+					<button class="btn bg-linear text-uppercase text-white rounded-pill px-5 py-2 font-size-14 font-weight-500 d-flex align-items-center">send message<span><img src="img/icons/arrow.png" class="ml-2 img-fluid"></span></button>
+				</div>
+			</div>
+		</form>		
+	</div>
+</section>
+
+<!-- [End of Contact Info Container] -->
+
+<!-- [Footer] -->
+
+<footer class="bg-outer-space" id="footer">
+	<div class="container py-5 px-xl-0 px-lg-0 px-md-0 px-sm-0">
+		<div class="row">
+			
+		</div>
+	</div>
+</footer>
+
+<!-- [End of Footer] -->
+
+<!-- [Scripts]	-->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+<!-- [End of Scripts] -->
+</body>
+</html>
